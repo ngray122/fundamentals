49f9003e8c59eaa084d557ac7a76d0275c903771
Nicole Gray <ngray122@gmail.com>
2021-11-29 20:00:58 -0500

    functions intermediate

--- functions_basic_2/basic2.py
+++ functions_basic_2/basic2.py
@@ -11,6 +11,13 @@ def countdown(start):
 
 print(countdown(9))
 
+# def countdown (num):
+#     output= []
+#     for i in range(num, -1, -1):
+#         output.append(i)
+#     return output
+
+
 
 # 2.  Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
 # Example: print_and_return([1,2]) should print 1 and return 2
@@ -39,9 +46,11 @@ print(first_plus_lng([1, 2, 3, 4, 5]))
 # 4. Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
 # Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
 # Example: values_greater_than_second([3]) should return False
-num_list = [5,2,3,2,1,4]
+num_list = [5]
 
 def val_greater(list):
+    if len(list) < 2:
+        return False
     count = 0
     new_list = []
     for item in range(len(list)):
@@ -53,6 +62,19 @@ def val_greater(list):
 
 print(val_greater(num_list))
 
+# num_list = [5,2,3,2,1,4]
+# def value_greater(list):
+#     if len(list) < 2:
+#         return False
+#     output = []
+#     for i in range(0,len(list)):
+#         if list[i] > list[1]:
+#             output.append(list[i])
+#     print(len(output))
+#     return(output)
+# value_greater(num_list)
+
+
 # 5.This Length, That Value - Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
 # Example: length_and_value(4,7) should return [7,7,7,7]
 # Example: length_and_value(6,2) should return [2,2,2,2,2,2]
@@ -62,3 +84,4 @@ def this_length(size, value):
     return new_list
 
 print(this_length(5, 8))
+print(this_length(7, 2))
